2ad221290cdb2f96f1c24c23dbdc560947a76be1
yingjen0106 <82910322+yingjen0106@users.noreply.github.com>
2021-10-16 19:56:23 -0400

    Add files via upload

---
+++ yingjen_chen_assignment2.js
@@ -0,0 +1,40 @@
+const itemsObject = [
+  { quantity: 1, price: 200 },
+  { quantity: 3, price: 350 },
+  { quantity: 5, price: 400 },
+];
+
+//new array - doubles
+let arr1 = [];
+for (const elem of itemsObject) {
+  let val = {};
+  val["quantity"] = elem["quantity"] * 2;
+  val["price"] = elem["price"] * 2;
+  arr1.push(val);
+}
+
+console.log(arr1);
+
+//contains item  quantity > 2 & price > 300
+let arr2 = [];
+for (const elem of itemsObject) {
+  if (elem["quantity"] > 2 && elem["price"] > 300) arr2.push(elem);
+}
+
+console.log(arr2);
+
+// total value
+const sum = itemsObject
+  .map((item) => item.price)
+  .reduce((prev, curr) => prev + curr, 0);
+
+console.log(sum);
+
+// string
+const string =
+  " Perhaps The Easiest-to-understand  Case  For Reduce Is  To Return The Sum Of  All The Elements In  An Array ";
+
+const newStr = string.replace(/[^a-zA-Z ]/g, "");
+const finalStr = newStr.replace(/\s+/g, " ").trim().toLowerCase();
+
+console.log(finalStr);
